adaf886dbadddcee530b11f0520541596f942c2f
Martysyuk.AI <Martysyuk.AI@ya.ru>
2021-01-26 16:24:20 +0300

    решил ДЗ 2 задача №2

---
+++ Homework2/homework2_2.py
@@ -0,0 +1,22 @@
+# Для списка реализовать обмен значений соседних элементов, т.е. Значениями обмениваются элементы с индексами
+# 0 и 1, 2 и 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте. Для заполнения списка
+# элементов необходимо использовать функцию input().
+
+my_list = [i for i in input('Введите любое количество цифр через пробел: ').split()]
+
+idx = 0
+if len(my_list) % 2 == 0:
+    while idx < len(my_list):
+        temp = my_list[0 + idx]
+        my_list[0 + idx] = my_list[1 + idx]
+        my_list[1 + idx] = temp
+        idx += 2
+else:
+    while idx < len(my_list) - 1:
+        temp = my_list[0 + idx]
+        my_list[0 + idx] = my_list[1 + idx]
+        my_list[1 + idx] = temp
+        idx += 2
+
+print(my_list)
+
