8f8b92117d66e3158d068b805212665f92d909dd
Sasha <martysyuk.ai@ya.ru>
2022-08-05 13:06:46 +0300

    initial

---
+++ Calсulator.py
@@ -0,0 +1,10 @@
+# Калькулятор
+import numexpr
+from colorama import init
+from colorama import Fore
+init()
+print(Fore.CYAN)
+expr = input('Введите математическое выражение')
+result = numexpr.evaluate(expr)
+print(Fore.GREEN)
+print(f'Результат: {result}')
